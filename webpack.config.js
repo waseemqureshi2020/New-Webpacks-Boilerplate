677552b4bc3ae20cc2bd335001b557f5eb50dfcb
Waseem Qureshi <81438868+waseemqureshi2020@users.noreply.github.com>
2022-12-13 05:30:56 +0500

    Configure Webpacks
    Configure Webpacks

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+/* eslint-disable indent */
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: './src/index.js',
+  },
+  devServer: {
+    static: './dist',
+  },
+   module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
+};
